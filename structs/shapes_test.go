6082a721b93921048f76bf7e2136e18a90b980be
Anders Poirel <andersjopo@gmail.com>
2024-08-26 16:04:04 +0200

    shapes & interfaces

---
+++ structs/shapes_test.go
@@ -0,0 +1,23 @@
+package structs
+
+import "testing"
+
+func TestArea(t *testing.T) {
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{Width: 12, Height: 6}, 72.0},
+		{Circle{Radius: 10}, 314.1592653589793},
+		{Triangle{Base: 12, Height: 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+
+}
